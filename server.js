fa591e2117242e7e27ba092dd498e10c88884466
Nimira43 <n.rabone72@aol.com>
2025-02-11 21:08:02 +0000

    By requiring Server from socket.io and then creating a new Server instance, we're linking Socket.IO with the existing HTTP server.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
+
+const { Server } = require('socket.io')
+const io = new Server(server)
+
 const port = 3000
 
 app.get('/', (req, res) => {
