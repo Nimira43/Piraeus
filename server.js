d19f99eb140d49d704dbe24b98f4dddb2fec80c8
Nimira43 <n.rabone72@aol.com>
2025-02-11 20:51:13 +0000

    Sending simple HTML to port 3000

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const http = require('http')
 const server = http.createServer(app)
 const port = 3000
 
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <h1>Piraeus</h1>
+      <hr />
+      <h4>Live Stock Market Prices and News</h4>
+    </div>
+  `)
+})
+
 server.listen(port, () => {
   console.log(`Listening on Port ${port}`)
 })
