e4df4a53cbb23413a2f214204ec672a54e4ca46b
Nimira43 <n.rabone72@aol.com>
2025-02-28 23:32:29 +0000

    Starting new mini project -> handling cors

--- 01-simple-socket-io-/server.js
+++ 01-simple-socket-io-/server.js
@@ -11,6 +11,7 @@ const port = 3000
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
+
 console.log('SERVER-SIDE')
 // Server Instance and Server Socket
 io.on('connection', socket => {
