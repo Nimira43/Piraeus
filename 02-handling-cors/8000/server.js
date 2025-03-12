ddea906fff7227ffea2eb52ace8312b0627cd4fb
Nimira43 <n.rabone72@aol.com>
2025-03-12 17:38:32 +0000

    Setting up server.js in 8000 to use Cors middleware to accept localhost 3000

--- 02-handling-cors/8000/server.js
+++ 02-handling-cors/8000/server.js
@@ -1,6 +1,11 @@
 const PORT = 8000
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 app.get('/', (req, res) => {
   res.send('This is port 8000.')
