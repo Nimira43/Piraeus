7fb84ed51529228dae8f7860ac8d9d25c8adc4f7
Nimira43 <n.rabone72@aol.com>
2025-03-23 16:18:58 +0000

    Looking at event - copy of section 1 made.

---
+++ 03-events/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+console.log('SERVER-SIDE')
+// Server Instance and Server Socket
+io.on('connection', socket => {
+  
+  console.log(`A new user connected with ID: ${socket.id}`)
+})
+
+server.listen(port, () => {
+  console.log(`Listening on Port ${port}`)
+})
