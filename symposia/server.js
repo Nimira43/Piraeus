202c4f0d95bb52a5a44cae234a4bfcc5488976c8
Nimira43 <n.rabone72@aol.com>
2025-07-29 13:01:14 +0100

    set up a listener for new connections on the server side

--- symposia/server.js
+++ symposia/server.js
@@ -3,9 +3,14 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
+const io = new Server(server)
 const port = 3000
 
 app.use(express.static('client'))
+
+io.on('connection', socket => {
+  console.log(`A user connected with ID: ${socket.id}`)
+})
 
 server.listen(port, () => {
   console.log(`Listening on Port ${port}`)
